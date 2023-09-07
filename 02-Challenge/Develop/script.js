b379f6d3581a05ea3e0b2ffb0eb1c46f6e89c6d0
Stretch Spelman <codingspelman@gmail.com>
2023-09-07 17:38:19 -0600

    added the rest of the code for the generator

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -5,6 +5,49 @@ var specialCharacters = ['!','@','#','$','%','^','&','*','(',')','_','+','[',']'
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 
 var generatePassword = function() {
+
+  var characterSet = [];
+
+    var passwordLength;
+
+    while (true) {
+      passwordLength = parseInt(prompt("Enter the desired password length (between 8 and 128):"));
+
+      if (passwordLength >= 8 && passwordLength <= 128) {
+        break; // Exits the loop if the input is within the desired range
+      } else {
+        alert("Password length must be between 8 and 128 characters.");
+      }
+    }
+
+    var includeLowercase = window.confirm("Would you like to include lowercase letters in your password?");
+    if (includeLowercase) {
+      characterSet = characterSet.concat(lowercaseLetters);
+    }
+
+    var includeUppercase = window.confirm("Would you like to include uppercase letters in your password?");
+    if (includeUppercase) {
+      characterSet = characterSet.concat(uppercaseLetters);
+    }
+
+    var includespecialCharacters = window.confirm("Would you like to include special characters in your password?");
+    if (includespecialCharacters) {
+      characterSet = characterSet.concat(specialCharacters);
+    }
+
+    var includeNumbers = window.confirm("Would you like to include numbers in your password?");
+    if (includeNumbers) {
+      characterSet = characterSet.concat(numbers);
+    }
+    
+    var password = "";
+
+    for (var i = 0; i < passwordLength; i++) {
+      var randomIndex = Math.floor(Math.random() * characterSet.length);
+      password += characterSet[randomIndex];
+    }
+
+    return password;
 }
 
 // Assignment Code
